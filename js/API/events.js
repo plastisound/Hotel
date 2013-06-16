bb2c30c1de1832380a2b4fa96899235049450e09
plastisound <pisd14@gmail.com>
2013-06-15 19:05:08 -0500

    s

--- js/API/events.js
+++ js/API/events.js
@@ -2,18 +2,20 @@
 $(document).ready(function (e) {
 	/*alert("s");*/
 	/*$('#regFot').hide();*/
-	if(!isLogin())
-		window.location.href = '#login';
-	$('#regSend').tap(function(){
-		if($('#regNom').val() !=='' && $('#regTel').val() !=='' && $('#regMail').val() !==''){
-			var nom = $('#regNom').val();
-			var tel = $('#regTel').val();
-			var mail = $('#regMail').val();
-			alert(nom +'\n'+ tel +'\n'+ mail );
-		}else{
-			alert('Todos los campos son requeridos');
-		}
-	});
+	document.addEventListener("deviceready",function(){
+		if(!isLogin())
+			window.location.href = '#login';
+		$('#regSend').tap(function(){
+			if($('#regNom').val() !=='' && $('#regTel').val() !=='' && $('#regMail').val() !==''){
+				var nom = $('#regNom').val();
+				var tel = $('#regTel').val();
+				var mail = $('#regMail').val();
+				navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel", "Aceptar" );
+			}else{
+				navigator.notification.alert('Todos los campos son requeridos', null, "Hotel", "Aceptar");
+			}
+		});
+	}, false);
 });
 function isLogin(){
 	return false;
