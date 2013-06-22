956181f9c3c955745c05b2fdd19c0abd734616c7
plastisound <pisd14@gmail.com>
2013-06-22 16:41:09 -0500

    22 junio

--- js/API/captura.js
+++ js/API/captura.js
@@ -1,9 +1,9 @@
 // Captura
 function tomarFoto(){
 	navigator.device.capture.captureImage(function(c){
-		var cant = c.lenght;
+		var cant = c.length;
 		for(i=0; i < cant; i++)
-			c[i].fullPath;
+			foto = c[i].fullPath;
 			$('#regFoto').attr('rel',foto);
 			$('#mFoto').html('<img src="'+foto+'" width="100%" />')/*.css("border","#000 solid 1px")*/
 	}, function(err){
